9ae96148adbd743cf6526d6d5b91b65a4853c9ae
danny <danny_karouw@hotmail.com>
2025-01-02 13:13:17 +0100

    Add Spring Boot backend project

---
+++ backend/danscloud/src/main/java/com/daden/danscloud/DanscloudApplication.java
@@ -0,0 +1,13 @@
+package com.daden.danscloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanscloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanscloudApplication.class, args);
+	}
+
+}
---
+++ backend/danscloud/src/test/java/com/daden/danscloud/DanscloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daden.danscloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanscloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
